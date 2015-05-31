8329d0919e3a9250853cf2008486e86d1c2ea787
Joe Letizia <joe@gust.com>
2015-05-31 11:01:43 -0400

    Adds R script to connect to DB

---
+++ R/connect_to_db.R
@@ -0,0 +1,3 @@
+drv <- dbDriver("PostgreSQL")
+con <- dbConnect(drv, dbname="teacher_data_new",host="localhost",port=5432,user="joeletizia")
+dbGetQuery(con, "select count(*), race_bl from nj_doe_teachers group by race_bl")
